e44add360d2b0aba2a847d2219a31161ff282eb1
Mathieu Guéry <mathguery@gmail.com>
2018-05-23 11:11:03 +0200

    Modified menu's diagram, added Bounce2 library, added buttons management

---
+++ menu_lcd/clavier.ino
@@ -0,0 +1,29 @@
+int clavier() {
+  debouncer1.update();
+  debouncer2.update();
+  debouncer3.update();
+  
+  int bit0 = debouncer1.read();
+  int bit1 = debouncer2.read();
+  int enable = debouncer3.read();
+
+  int input = enable*100+bit1*10+bit0;
+
+  switch (input) {
+    case 100:
+      return 1;
+
+    case 101:
+      return 2;
+
+    case 110:
+      return 3;
+
+    case 111:
+      return 4;
+
+    default:
+      return 0;
+  }
+}
+
---
+++ menu_lcd/menu_lcd.ino
@@ -0,0 +1,47 @@
+#include "Bounce2.h"
+#include <LiquidCrystal.h>
+
+#define MAX_BATTERY 774.0
+
+//décodeur boutons
+#define BP0 16 
+#define BP1 15
+#define BPEN 17
+
+Bounce debouncer1 = Bounce();
+Bounce debouncer2 = Bounce();
+Bounce debouncer3 = Bounce();
+
+LiquidCrystal lcd(4,5,6,7,8,9);
+
+void setup() {
+  
+  lcd.begin(8,2);
+
+  pinMode(BP0, INPUT_PULLUP);
+  debouncer1.attach(BP0);
+  debouncer1.interval(5);
+  
+  pinMode(BP1, INPUT_PULLUP);
+  debouncer2.attach(BP1);
+  debouncer2.interval(5);
+
+  pinMode(BPEN, INPUT_PULLUP);
+  debouncer3.attach(BPEN);
+  debouncer3.interval(5);
+  
+  // Initilisation : afficher batterie
+  lcd.print("BATTERY");
+  lcd.setCursor(0,1);
+  lcd.print(((float)analogRead(A0) * 100.0)/MAX_BATTERY);
+  lcd.print('%');
+  delay(3000);
+  lcd.clear();
+  delay(100);
+}
+
+void loop() {
+
+  clavier();
+
+}
