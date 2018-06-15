8cf716a7e5bfad3a0a276fdcddd728683dce5907
benoitphilippe <benoit.philippe@utt.fr>
2018-06-15 09:32:21 +0200

    road to end

--- map/follow_road.py
+++ map/follow_road.py
@@ -31,6 +31,18 @@ def follow_road(file_name, out_file_name, step=50):
                 r.raise_for_status()
             for road_coords in datas["routes"][0]["geometry"]["coordinates"]:
                 road["geometry"]["coordinates"].append(road_coords)
+        # last request for remaining coords
+        coords = journey["geometry"]["coordinates"][len(journey["geometry"]["coordinates"]) - 1] # last coords
+        request_string = "http://router.project-osrm.org/route/v1/driving/{0:.6f},{1:.6f};{2:.6f},{3:.6f}?geometries=geojson&overview=full".format(
+            last_coords[0], last_coords[1], coords[0], coords[1]
+        ) 
+        r = requests.get(request_string)
+        if r.status_code == 200:
+            datas = r.json()
+        else:
+            r.raise_for_status()
+        for road_coords in datas["routes"][0]["geometry"]["coordinates"]:
+            road["geometry"]["coordinates"].append(road_coords)
         with open(out_file_name, 'w') as fout:
             json.dump(road, fout)
             print("done :)")
@@ -41,4 +53,9 @@ def follow_road(file_name, out_file_name, step=50):
 
 
 if __name__ == "__main__":
-    follow_road(sys.argv[1], sys.argv[2])
+    if len(sys.argv) == 3:
+        follow_road(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        follow_road(sys.argv[1], sys.argv[2],step=sys.argv[3])
+    else:
+        print("wrong number of argument !")
